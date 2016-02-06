37fdc1b619a2ae2d951713895dabe84792d923df
Eugene Cheung <echeung@infusion.com>
2016-02-06 00:03:23 -0500

    Basic router

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { IndexLink, Link } from 'react-router';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <IndexLink to='/'>Home</IndexLink>
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
+
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-const css = require('./styles/styles.scss');
+import App from './components/App';
+import NotFound from './components/NotFound';
+import LandingPage from './components/LandingPage';
+import Login from './components/Login';
+import Register from './components/Register';
 
-render(
-  <div>Hello World</div>,
-  document.querySelector('main')
-);
+require('./styles/styles.scss');
+
+render((
+  <Router history={browserHistory}>
+    <Route path='/' component={App}>
+      <IndexRoute component={LandingPage} />
+      <Route path='login' component={Login} />
+      <Route path='register' component={Register} />
+      <Route path='*' component={NotFound} />
+    </Route>
+  </Router>
+), document.querySelector('main'));
