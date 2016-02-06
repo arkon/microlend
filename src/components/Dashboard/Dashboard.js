8894e9ab8daa6a0125af4576a28feefb6ef438ff
Eugene Cheung <echeung@infusion.com>
2016-02-06 12:44:07 -0500

    Placeholder login/password views

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import { Grid, Row, Col } from '../Grid/Grid';
+
 class Dashboard extends React.Component {
   render () {
     return (
-      <div>
+      <div className='container'>
         <h1>Dashboard</h1>
 
-        <Link to='/dashboard/invest'>Invest</Link>
-        <Link to='/dashboard/loan'>Loan</Link>
+        <p>My status</p>
+
+        <Grid>
+          <Row>
+            <Col>
+              <Link to='/dashboard/invest'>Invest</Link>
+            </Col>
+            <Col>
+              <Link to='/dashboard/loan'>Loan</Link>
+            </Col>
+          </Row>
+        </Grid>
 
         {this.props.children}
       </div>
--- src/components/User/Register.js
+++ src/components/User/Register.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 class Register extends React.Component {
   render () {
-    return <p>Register</p>;
+    return (
+      <div className='container'>
+        <div className='user_form'>
+          <h1>Register</h1>
+
+          <div>
+            <label>Username</label>
+            <input type='text' required />
+          </div>
+
+          <div>
+            <label>Email address</label>
+            <input type='email' required />
+          </div>
+
+          <div>
+            <label>Password</label>
+            <input type='password' required />
+          </div>
+
+          <div>
+            <label>Confirm Password</label>
+            <input type='password' required />
+          </div>
+
+          <button>Register</button>
+        </div>
+      </div>
+    );
   }
 }
 
