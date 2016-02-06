6270ba581cd300b8bd8f44ad8b29f294531f910d
Spencer Elliott <me@elliottsj.com>
2016-02-05 20:44:08 -0500

    Hello World

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # microlend
+
+### Installing
+
+Requirements:
+* Node.js 5
+
+```
+npm install
+```
+
+### Running
+
+```
+npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: '/',
+    publicPath: '/assets',
+    filename: 'bundle.js',
+    pathinfo: true,
+  },
+  devtool: 'eval',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        include: path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'static'),
+    stats: { chunkModules: false },
+  },
+};
