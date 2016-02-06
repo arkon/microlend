6d00b2f3fea6afc625a2a0372a0abedbb1a2b985
Eugene Cheung <echeung@infusion.com>
2016-02-06 17:38:39 -0500

    Autosuggest styling

--- src/components/Borrow/Borrow.js
+++ src/components/Borrow/Borrow.js
@@ -4,7 +4,7 @@ import AutosuggestHighlight from 'autosuggest-highlight';
 
 const friends = [
   {
-    name: 'Eugene Chung',
+    name: 'Eugene Cheung',
     img: 'eugene.jpg'
   },
   {
@@ -21,7 +21,7 @@ const friends = [
   }
 ];
 
-function getSuggestions(value) {
+function getSuggestions (value) {
   const inputValue = value.trim().toLowerCase();
 
   return value.length === 0 ? [] : friends.filter(person =>
@@ -29,50 +29,47 @@ function getSuggestions(value) {
   );
 }
 
-function getSuggestionValue(suggestion){
+function getSuggestionValue (suggestion) {
   return suggestion.name;
 }
 
-function renderSuggestion(suggestion, { value, valueBeforeUpDown }){
-
+function renderSuggestion (suggestion, { value, valueBeforeUpDown }) {
   const suggestionText = suggestion.name;
   const query = (valueBeforeUpDown || value).trim();
   const matches = AutosuggestHighlight.match(suggestionText, query);
   const parts = AutosuggestHighlight.parse(suggestionText, matches);
 
-  const profileImage = '/img/users/' + suggestion.img;
-
-  return (<span className='suggestion-content'>
-    <img className='profile-image' src={profileImage} />
-    <span className="name">
-      {
-        parts.map((part, index) => {
-          const className = part.highlight ? 'highlight' : null;
+  return (
+    <span className='react-autosuggest__suggestion__content'>
+      <img className='react-autosuggest__suggestion__content__photo' src={`/img/users/${suggestion.img}`} />
+      <span className='react-autosuggest__suggestion__content__name'>
+        { parts.map((part, index) => {
+          const className = part.highlight ? 'react-autosuggest__suggestion__content__name--highlight' : null;
 
           return (
             <span className={className} key={index}>{part.text}</span>
           );
-        })
-      }
+        }) }
+      </span>
     </span>
-  </span>);
+  );
 }
 
 class Borrow extends React.Component {
-
-  constructor(){
-    super();
+  constructor (props) {
+    super(props);
 
     this.state = {
-      value:'',
+      value: '',
       suggestions: getSuggestions('')
     };
-
-
   }
 
   onChange = (event, { newValue }) => {
-    this.setState({value: newValue});
+    this.setState({
+      value: newValue
+    });
+
     console.log('Search changed');
   };
 
@@ -82,13 +79,12 @@ class Borrow extends React.Component {
     });
   };
 
-  onSuggestionSelected(event, { suggestion, suggestionValue, method }){
+  onSuggestionSelected (event, { suggestion, suggestionValue, method }) {
     console.log(suggestion);
     console.log(suggestionValue);
   }
 
   render () {
-
     const inputProps = {
       placeholder: 'Search in your network',
       value: this.state.value,
@@ -96,11 +92,9 @@ class Borrow extends React.Component {
     };
 
     return (
-      <div className="container">
-        <h2>Borrow</h2>
-        <p>
-          Borrow with a group to reduce your interest rate.
-        </p>
+      <div className='container'>
+        <h1>Borrow</h1>
+        <p>Borrow with a group to reduce your interest rate.</p>
 
         <label>Amount ($)</label>
         <input type='number' required />
@@ -109,15 +103,16 @@ class Borrow extends React.Component {
         <table>
           <tbody>
             <tr>
-              <td><Autosuggest suggestions={this.state.suggestions}
-                getSuggestionValue={getSuggestionValue}
-                renderSuggestion={renderSuggestion}
-                inputProps={inputProps}
-                onSuggestionsUpdateRequested = {this.onSuggestionsUpdateRequested}
-                onSuggestionSelected = {this.onSuggestionSelected}
-                /></td>
+              <td>
+                <Autosuggest suggestions={this.state.suggestions}
+                  getSuggestionValue={getSuggestionValue}
+                  renderSuggestion={renderSuggestion}
+                  inputProps={inputProps}
+                  onSuggestionsUpdateRequested={this.onSuggestionsUpdateRequested}
+                  onSuggestionSelected={this.onSuggestionSelected} />
+              </td>
             </tr>
-            </tbody>
+          </tbody>
         </table>
 
         <button>Submit</button>
