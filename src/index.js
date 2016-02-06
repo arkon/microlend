c1677f2247b145899344ca6beb7fae82e4f1672a
Eugene Cheung <echeung@infusion.com>
2016-02-05 21:52:40 -0500

    Add basic Sass files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-const css = require('./styles.scss');
+const css = require('./styles/styles.scss');
 
 render(
   <div>Hello World</div>,
