7f3b8e0c72bfc11f1d2de7068c506f60034d8070
Eugene Cheung <echeung@infusion.com>
2016-02-06 11:17:38 -0500

    Boilerplate Redux stuff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistory, routeReducer } from 'react-router-redux';
+
+import reducers from './reducers';
 
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
@@ -11,17 +16,32 @@ import Dashboard from './components/Dashboard/Dashboard';
 
 require('./styles/styles.scss');
 
+const reducer = combineReducers(Object.assign({}, reducers, {
+  routing: routeReducer
+}));
+
+// Sync dispatched route actions to the history
+const reduxRouterMiddleware = syncHistory(browserHistory);
+const createStoreWithMiddleware = applyMiddleware(reduxRouterMiddleware)(createStore);
+
+const store = createStoreWithMiddleware(reducer);
+
+// Required for replaying actions from devtools to work
+reduxRouterMiddleware.listenForReplays(store);
+
 render((
-  <Router history={browserHistory}>
-    <Route path='/' component={App}>
-      <IndexRoute component={LandingPage} />
-      <Route path='login' component={Login} />
-      <Route path='register' component={Register} />
-      <Route path='dashboard' component={Dashboard}>
-        <Route path='invest' component={Login} />
-        <Route path='loan' component={Register} />
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path='/' component={App}>
+        <IndexRoute component={LandingPage} />
+        <Route path='login' component={Login} />
+        <Route path='register' component={Register} />
+        <Route path='dashboard' component={Dashboard}>
+          <Route path='invest' component={Login} />
+          <Route path='loan' component={Register} />
+        </Route>
+        <Route path='*' component={NotFound} />
       </Route>
-      <Route path='*' component={NotFound} />
-    </Route>
-  </Router>
-), document.querySelector('main'));
+    </Router>
+  </Provider>
+), document.getElementById('app'));
