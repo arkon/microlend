9ea73371d703dbb7479244f5c51db8e0d71e3f8d
Eugene Cheung <echeung@infusion.com>
2016-02-06 15:08:28 -0500

    Remove deprecated Grid component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-import { Grid, Row, Col } from '../Grid/Grid';
+import { Row, Col } from '../Grid/Grid';
 
 class Dashboard extends React.Component {
   render () {
@@ -12,41 +12,37 @@ class Dashboard extends React.Component {
         <section className='dashboard__section'>
           <h2>My status</h2>
 
-          <Grid>
-            <Row>
-              <Col>
-                Money invested
-              </Col>
-              <Col>
-                Money borrowed
-              </Col>
-            </Row>
-          </Grid>
+          <Row>
+            <Col>
+              Money invested
+            </Col>
+            <Col>
+              Money borrowed
+            </Col>
+          </Row>
         </section>
 
         <section className='dashboard__section'>
           <h2>Refinance</h2>
 
-          <Grid>
-            <Row>
-              <Col>
-                <Link to='/dashboard/invest'>
-                  <div className='dashboard__tile'>
-                    <h3>Invest</h3>
-                    <p>Contribute to a global pool to get promised return.</p>
-                  </div>
-                </Link>
-              </Col>
-              <Col>
-                <Link to='/borrow'>
-                  <div className='dashboard__tile'>
-                    <h3>Borrow</h3>
-                    <p>Borrow in a group to get lower interest rates.</p>
-                  </div>
-                </Link>
-              </Col>
-            </Row>
-          </Grid>
+          <Row>
+            <Col>
+              <Link to='/dashboard/invest'>
+                <div className='dashboard__tile'>
+                  <h3>Invest</h3>
+                  <p>Contribute to a global pool to get promised return.</p>
+                </div>
+              </Link>
+            </Col>
+            <Col>
+              <Link to='/borrow'>
+                <div className='dashboard__tile'>
+                  <h3>Borrow</h3>
+                  <p>Borrow in a group to get lower interest rates.</p>
+                </div>
+              </Link>
+            </Col>
+          </Row>
 
           <div className='dashboard__subcontent'>
             {this.props.children}
